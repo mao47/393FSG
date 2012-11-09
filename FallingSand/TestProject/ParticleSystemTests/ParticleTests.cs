416e5d2ed4b3d58abefc4d09d3b3da934696bacd
Michael Oswalt <mao47@case.edu>
2012-11-08 22:13:53 -0500

    Added nunit runners package. Still can't quite get it to work

---
+++ FallingSand/TestProject/ParticleSystemTests/ParticleTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FallingSand.Particles;
+using Microsoft.Xna.Framework;
+
+namespace TestProject.ParticleSystemTests
+{
+    [TestFixture]
+    public class ParticleTests
+    {
+        [Test]
+        public void TestGetColor()
+        {
+            var sand = new Particle(Vector2.Zero, Vector2.Zero, Particle_Type.Sand);
+            var wall = new Particle(Vector2.Zero, Vector2.Zero, Particle_Type.Wall);
+
+            var sandc = sand.getColor();
+            var wallc = sand.getColor();
+
+            Assert.AreNotEqual(sandc, Color.White);
+            Assert.AreNotEqual(wallc, Color.White);
+        }
+    }
+}
